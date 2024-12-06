9dc78b99ca76f0fc5d0259ef6c982de47ecffb36
AntonioBerna <asukeasukai12@gmail.com>
2024-12-06 08:55:15 +0100

    style :recycle:

--- days/src/bin/day03.rs
+++ days/src/bin/day03.rs
@@ -4,15 +4,13 @@ pub const GOOD_WEIGHT: f32 = 1.0;
 pub const BAD_WEIGHT: f32 = 2.0;
 
 pub fn is_nice(good_deeds: u32, bad_deeds: u32) -> bool {
-    // Calculate the ratio of good deeds to total deeds
-    // Any ratio greater than 0.75 is considered nice
-    // e.g. 10 good deeds and 2 bad deeds =
-    // (10 * 1) / (10 * 1) + (2 * 2) = 10 / 14 = 0.714... (not nice)
-    // If both good and bad deeds are 0, the child is naughty
     if good_deeds == 0 && bad_deeds == 0 {
-        false
-    } else {
-        let (good_deeds, bad_deeds) = (good_deeds as f32, bad_deeds as f32);
-        good_deeds * GOOD_WEIGHT / (good_deeds * GOOD_WEIGHT + bad_deeds * BAD_WEIGHT) >= 0.75
+        return false;
     }
+
+    let good_deeds = good_deeds as f32 * GOOD_WEIGHT;
+    let bad_deeds = bad_deeds as f32 * BAD_WEIGHT;
+    let ratio = good_deeds / (good_deeds + bad_deeds);
+
+    ratio >= 0.75
 }
